05b9d7e6ec1413137ce2518e039092a0cbb23d31
danielhennig <danielhennig10@gmail.com>
2025-03-16 17:42:21 -0300

    Adicionar função para somar elementos de um array e tratamento de erros

--- 1️⃣ Somar Todos os Elementos de um Array Unidimensional/index.js
+++ 1️⃣ Somar Todos os Elementos de um Array Unidimensional/index.js
@@ -1,1 +1,17 @@
-console.log('Hello, world!');
+function somaArray(numbers) {
+    for (let num of numbers) {
+        if (typeof num !== 'number' || isNaN(num)) {
+            throw new Error("Valor inválido detectado");
+        }
+    }
+
+    return numbers.reduce((soma, num) => soma + num, 0);
+}
+
+const array = [1, 2, 3, 4, 5];
+
+try {
+    console.log(somaArray(array)); 
+} catch (error) {
+    console.error(error.message);
+}
